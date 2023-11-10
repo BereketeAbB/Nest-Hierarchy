a55d3cf27802fff508d81c4bbc766d93e2192c85
BereketeAb <bereketeabbirku@gmail.com>
2023-11-10 17:44:01 +0300

    almost done

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
     UserModule, 
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 5432,
-      username: 'postgres',
-      password: 'root',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
       database: 'orga-structure',
       // entities: [__dirname + '/**/*.entity{.ts, .js}'],
       entities: [User],
       logging: true,
       synchronize: true,
     }),
-    
   ],
   controllers: [AppController],
   providers: [AppService],
